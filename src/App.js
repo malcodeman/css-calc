d315197865778c93e6352fbd6a7066403e2f7d8c
malcodeman <26983729+malcodeman@users.noreply.github.com>
2020-07-15 23:49:52 +0200

    Adds px/rem conversion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 function App() {
-  return <h1>CSS calc</h1>;
+  const [rootFontSize, setRootFontSize] = React.useState("16");
+  const [px, setPx] = React.useState("16");
+  const [rem, setRem] = React.useState("1");
+
+  function handlePxOnChange(e) {
+    const value = e.target.value;
+
+    setPx(value);
+    setRem(value / Number(rootFontSize));
+  }
+
+  function handleRemOnChange(e) {
+    const value = e.target.value;
+
+    setRem(value);
+    setPx(value * Number(rootFontSize));
+  }
+
+  return (
+    <div>
+      <h1>CSS calc</h1>
+      <div>
+        <input
+          type="text"
+          value={rootFontSize}
+          onChange={(e) => setRootFontSize(e.target.value)}
+        />
+        <span>root font size</span>
+      </div>
+      <div>
+        <input type="text" value={px} onChange={handlePxOnChange} />
+        <span>px</span>
+      </div>
+      <div>
+        <input type="text" value={rem} onChange={handleRemOnChange} />
+        <span>rem</span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
