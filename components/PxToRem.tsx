759b163e287f52361b2c6a98625861b0bca66181
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-02-06 14:47:32 +0100

    Adds px to rem calculations

--- components/PxToRem.tsx
+++ components/PxToRem.tsx
@@ -1,5 +1,6 @@
-import { Box, Input, InputWrapper } from "@mantine/core";
+import { Box, Input, InputWrapper, Text } from "@mantine/core";
 import { useForm } from "react-hook-form";
+import { toString } from "ramda";
 
 const defaultValues = {
   px: "16",
@@ -8,18 +9,52 @@ const defaultValues = {
 };
 
 const PxToRem = () => {
-  const { register } = useForm({ defaultValues });
+  const { register, setValue, getValues } = useForm({ defaultValues });
+
+  function handleOnPxChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const px = parseFloat(e.target.value);
+    const rootFontSize = parseFloat(getValues("rootFontSize"));
+    setValue("rem", toString(px / rootFontSize));
+  }
+
+  function handleOnRemChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const rem = parseFloat(e.target.value);
+    const rootFontSize = parseFloat(getValues("rootFontSize"));
+    setValue("px", toString(rem * rootFontSize));
+  }
+
+  function handleOnRootFontSizeChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const rootFontSize = parseFloat(e.target.value);
+    const px = parseFloat(getValues("px"));
+    setValue("rem", toString(px / rootFontSize));
+  }
 
   return (
     <Box>
-      <InputWrapper id="px" label="px">
-        <Input id="px" {...register("px")} />
+      <InputWrapper id="px" label="Pixel">
+        <Input
+          id="px"
+          {...register("px")}
+          onChange={handleOnPxChange}
+          rightSection={<Text>px</Text>}
+          mb={"sm"}
+        />
       </InputWrapper>
-      <InputWrapper id="rem" label="rem">
-        <Input id="rem" {...register("rem")} />
+      <InputWrapper id="rem" label="REM">
+        <Input
+          id="rem"
+          {...register("rem")}
+          onChange={handleOnRemChange}
+          rightSection={<Text>rem</Text>}
+          mb={"sm"}
+        />
       </InputWrapper>
-      <InputWrapper id="rootFontSize" label="root font size">
-        <Input id="rootFontSize" {...register("rootFontSize")} />
+      <InputWrapper id="rootFontSize" label="Root Font Size">
+        <Input
+          id="rootFontSize"
+          {...register("rootFontSize")}
+          onChange={handleOnRootFontSizeChange}
+        />
       </InputWrapper>
     </Box>
   );
