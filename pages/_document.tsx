cd3e5bb368a6bb9b9be45568bcce6cc1aa33bd1c
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-02-06 15:08:42 +0100

    Adds document file

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { createGetInitialProps } from "@mantine/next";
+
+const getInitialProps = createGetInitialProps();
+
+class MyDocument extends Document {
+  static getInitialProps = getInitialProps;
+  render() {
+    return (
+      <Html>
+        <Head lang="en">
+          <meta charSet="utf-8" />
+          <meta
+            name="og:description"
+            content="css calc - simple css calculations"
+          />
+          <meta
+            name="description"
+            content="css calc - simple css calculations"
+          />
+          <meta property="og:image" content="opengraph.png"></meta>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <div id="root" />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
