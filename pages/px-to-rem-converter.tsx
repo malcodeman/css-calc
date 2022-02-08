99fd1eb18ddf72aa2bab6cf06192e71f6769c0cf
Amer K <26983729+malcodeman@users.noreply.github.com>
2022-02-08 15:39:59 +0100

    Adds titles to converter pages

--- pages/px-to-rem-converter.tsx
+++ pages/px-to-rem-converter.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import Head from "next/head";
 import { Box, Input, InputWrapper, Text, Title } from "@mantine/core";
 import { useForm } from "react-hook-form";
 import { toString } from "ramda";
@@ -30,36 +32,41 @@ const PxToRemConverter = () => {
   }
 
   return (
-    <Box>
-      <Title order={1} align={"center"} mb={"lg"}>
-        Pixel to REM
-      </Title>
-      <InputWrapper id="px" label="Pixel">
-        <Input
-          id="px"
-          {...register("px")}
-          onChange={handleOnPxChange}
-          rightSection={<Text>px</Text>}
-          mb={"sm"}
-        />
-      </InputWrapper>
-      <InputWrapper id="rem" label="REM">
-        <Input
-          id="rem"
-          {...register("rem")}
-          onChange={handleOnRemChange}
-          rightSection={<Text>rem</Text>}
-          mb={"sm"}
-        />
-      </InputWrapper>
-      <InputWrapper id="rootFontSize" label="Root Font Size">
-        <Input
-          id="rootFontSize"
-          {...register("rootFontSize")}
-          onChange={handleOnRootFontSizeChange}
-        />
-      </InputWrapper>
-    </Box>
+    <React.Fragment>
+      <Head>
+        <title>Pixel to REM Convertor</title>
+      </Head>
+      <Box>
+        <Title order={1} align={"center"} mb={"lg"}>
+          Pixel to REM
+        </Title>
+        <InputWrapper id="px" label="Pixel">
+          <Input
+            id="px"
+            {...register("px")}
+            onChange={handleOnPxChange}
+            rightSection={<Text>px</Text>}
+            mb={"sm"}
+          />
+        </InputWrapper>
+        <InputWrapper id="rem" label="REM">
+          <Input
+            id="rem"
+            {...register("rem")}
+            onChange={handleOnRemChange}
+            rightSection={<Text>rem</Text>}
+            mb={"sm"}
+          />
+        </InputWrapper>
+        <InputWrapper id="rootFontSize" label="Root Font Size">
+          <Input
+            id="rootFontSize"
+            {...register("rootFontSize")}
+            onChange={handleOnRootFontSizeChange}
+          />
+        </InputWrapper>
+      </Box>
+    </React.Fragment>
   );
 };
 
